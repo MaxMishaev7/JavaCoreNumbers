cc37cd7e04604ffdabea3b57acf0748b35072de6
MaxMishaev <maxmishaev74@gmail.com>
2024-02-20 16:17:41 +0300

    The project has been modified in accordance with the instructions

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,41 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
 
-        int[] numList = {1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4};
-        int[] numbers = new int[10];
-        int count = 0;
-        for (int i = 0; i < numList.length; i++) {
-            if (numList[i] > 0 && numList[i] % 2 == 0) {
-                numbers[count] = numList[i];
-                count++;
-            }
-        }
+//        int[] numList = {1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4};
+//        int[] numbers = new int[10];
+//        int count = 0;
+//        for (int i = 0; i < numList.length; i++) {
+//            if (numList[i] > 0 && numList[i] % 2 == 0) {
+//                numbers[count] = numList[i];
+//                count++;
+//            }
+//        }
+
+
         //System.out.println(Arrays.toString(numbers));
 
-        int[] newNumArray = new int[count];
-        for (int i = 0; i < count; i++) {
-            newNumArray[i] = numbers[i];
-        }
-        System.out.println(Arrays.toString(newNumArray));
-        Arrays.sort(newNumArray);
-        System.out.println(Arrays.toString(newNumArray));
+//        int[] newNumArray = new int[count];
+//        for (int i = 0; i < count; i++) {
+//            newNumArray[i] = numbers[i];
+//        }
+//        System.out.println(Arrays.toString(newNumArray));
+//        Arrays.sort(newNumArray);
+//        System.out.println(Arrays.toString(newNumArray));
+
+          List<Integer> numList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+          List<Integer> newNumList = new ArrayList<>();
+          for (Integer num : numList) {
+              if (num > 0 && num % 2 == 0) {
+                  newNumList.add(num);
+              }
+          }
+          Collections.sort(newNumList);
+          System.out.println(newNumList);
 
     }
 }
--- src/StreamMain.java
+++ src/StreamMain.java
@@ -4,12 +4,11 @@ import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
-        int[] numList = {1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4};
-        List<Integer> streamNumList = Arrays.stream(numList)
+        List<Integer> numList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> streamNumList = numList.stream()
                 .filter(x -> x > 0)
                 .filter(x -> x % 2 == 0)
                 .sorted()
-                .boxed()
                 .collect(Collectors.toList());
         System.out.println("Stream API");
         System.out.println(streamNumList);
